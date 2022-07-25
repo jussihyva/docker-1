c33796c0aef09e978d9d3584566519a5b0643cf3
Juhani Kauppi <jkauppi@student.hive.fi>
2022-07-25 14:15:29 +0300

    ex02. Create new function implemented.

--- 01_dockerfiles/ex02/app/test/helpers/console_cmd.rb
+++ 01_dockerfiles/ex02/app/test/helpers/console_cmd.rb
@@ -1,8 +1,8 @@
 article = Article.new(title: "Hello Rails", body: "I am on Rails!")
 article.save
 article
+Article.all
 article = Article.new(title: "Hello Hive", body: "I am working for docker-1 project!")
 article.save
 article
-Article.find(1)
 Article.all
